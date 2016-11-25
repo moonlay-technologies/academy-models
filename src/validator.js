3bf86cc94cd2565af075363382bf69ee08abb3ea
Tris Setiawan <tris.setiawan@yahoo.com>
2016-11-25 12:22:31 +0700

    add auth models

---
+++ src/validator.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+        auth: {
+                account: require("./auth/account-validator"),
+                profile: require("./auth/profile-validator"),
+                role: require("./auth/role-validator")
+        }
+};
